7e92bc255b5613c25c8f2e3bbb0d0b4bf18b6cc2
utopi-a <bridgestone.amatsu0228@gmail.com>
2025-01-27 13:09:29 +0900

    feat: スライスの章終了

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -15,6 +15,8 @@ fn main() {
     println!("{}", s3);
 
     references_and_borrowing();
+
+    slice_type();
 }
 
 fn takes_ownership(some_string: String) -> String {
@@ -36,4 +38,28 @@ fn references_and_borrowing() {
 fn change_calculate_length(s: &mut String) -> usize {
     s.push_str(", mwah");
     s.len()
+}
+
+fn slice_type() {
+    let mut s = String::from("hello world");
+    let word = first_word(&s);
+
+    println!("{}", word);
+
+    let a = [1, 2, 3, 4, 5];
+    let a_slice = &a[3..];
+
+    println!("{:?}", a_slice);
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
