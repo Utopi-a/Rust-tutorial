a3dedb9ff259cf75f547dc33e86b00de0227ecb8
utopi-a <bridgestone.amatsu0228@gmail.com>
2025-01-27 12:18:24 +0900

    feat: 制御フローの章を終えた

--- normal-programming-concept/src/main.rs
+++ normal-programming-concept/src/main.rs
@@ -7,6 +7,13 @@ fn main() {
     println!("yの値は{}です", y);
     let result = test_function(10, 'd');
     println!("resultの値は{}です", result);
+
+    flow_control();
+    loop_test();
+    for_test();
+    for index in 1..10 {
+        println!("{}", fibonacci(index));
+    }
 }
 
 fn test_function(num: i32, unitLabel: char) -> i32 {
@@ -14,4 +21,65 @@ fn test_function(num: i32, unitLabel: char) -> i32 {
     2345
     // なんの意味もないコメントです
     // コメント，たのし〜
+}
+
+fn flow_control() {
+    let number = 204;
+    const FILTER_NUMBER: i32 = 234;
+
+    if number < FILTER_NUMBER {
+        println!("小さいよ，{}よりも", FILTER_NUMBER);
+    } else {
+        println!("以上だよ，{}よりも", FILTER_NUMBER);
+    }
+
+    if number != 0 {
+        println!("numberは0ではないなんらかである");
+    }
+
+    let test = if number == 234 { true } else { false };
+    println!("testの値は{}です", test);
+}
+
+fn loop_test() {
+    let mut counter = 0;
+    'counting_up: loop {
+        counter += 1;
+        if counter == 10 {
+            break 'counting_up;
+        }
+        println!("{}", counter);
+    }
+    println!("{}", counter);
+
+    while counter != 0 {
+        println!("{}!", counter);
+
+        counter -= 1;
+    }
+
+    println!("射爆了");
+}
+
+fn for_test() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("{}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("打ち上げ");
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 || n == 2 {
+        return 1;
+    }
+
+    fibonacci(n - 1) + fibonacci(n - 2)
 }
