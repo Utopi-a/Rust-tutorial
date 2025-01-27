cac939a544c87b05303fad4eeb9958df72c47518
utopi-a <bridgestone.amatsu0228@gmail.com>
2025-01-27 11:05:52 +0900

    feat: 関数の章を終えた

---
+++ normal-programming-concept/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("yの値は{}です", y);
+    let result = test_function(10, 'd');
+    println!("resultの値は{}です", result);
+}
+
+fn test_function(num: i32, unitLabel: char) -> i32 {
+    println!("他の関数だよ〜 {} {}", num, unitLabel);
+    2345
+}
