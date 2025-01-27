43b908d228ab71d7c31fe17a13d7aa4fdfb67d3f
utopi-a <bridgestone.amatsu0228@gmail.com>
2025-01-27 10:22:49 +0900

    feat: 変数の項目終了

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     let x = 5;
-    println!("xの値は: {}", x);
-    x = 6;
-    println!("xの値は: {}", x);
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("内部スコープにおけるXの値は: {}", x);
+    }
+
+    println!("外部スコープにおけるXの値は: {}", x);
 }
